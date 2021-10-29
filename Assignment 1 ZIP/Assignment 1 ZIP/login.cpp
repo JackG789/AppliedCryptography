2378a6ca1423df059eef4429c59dcc09bbf36a9a
JackG789 <55397601+JackG789@users.noreply.github.com>
2021-10-29 14:18:29 +0100

    Update login.cpp
    test

--- Assignment 1 ZIP/Assignment 1 ZIP/login.cpp
+++ Assignment 1 ZIP/Assignment 1 ZIP/login.cpp
@@ -2,8 +2,30 @@
 #include "authlib.h"
 
 int main() {
+  string userName;
+  string userPassword;
+  string storedUserData[];
   bool auth = true;
- 
+  int loginAttempt =0;
+  //todo read names from file into usernames array
+  ifstream MyReadFile("usernames.txt");
+  while (getline (MyReadFile, storedUserData, ":")) {
+  cout << storedUserData;
+  }
+  MyReadFile.close();
+
+  while(loginAttempt < 3){
+    cout << "please enter usename: ";
+    cin >> userName;
+    cout << "please enter password: ";
+    cin >> userPassword;
+  if(  userName != "user"){ //compare usernames entered to usernames from file
+    auth == false;     
+  }
+  loginAttempt = loginAttempt +1; //add to sign in attempt
+
   if (auth) authenticated("user");
+ //sign in
   else rejected("user");
+  }
 }
