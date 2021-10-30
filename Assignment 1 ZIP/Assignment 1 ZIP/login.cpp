759658997ee2cb78bd6991d31f2b2cbb4987fec9
JackG789 <55397601+JackG789@users.noreply.github.com>
2021-10-30 20:06:27 +0100

    Update login.cpp
    yes

--- Assignment 1 ZIP/Assignment 1 ZIP/login.cpp
+++ Assignment 1 ZIP/Assignment 1 ZIP/login.cpp
@@ -1,43 +1,103 @@
 #include <iostream>
-#include <iostream>
+
 #include "authlib.h"
 
+#include <fstream>
+
+
+
+using namespace std;
+
+
+
 int main() {
+
   std::string user;
+
   std::string userPassword;
-  string storedUserData[];
-  std::int i;
+
+  std::string storedUserData;
+
+ 
+
+  //std::string MyReadFile ="Desktop/passwords.txt";
+
+  int i=0;
+
   
+
   bool auth = false;
+
   int loginAttempt =0;
+
   //todo read names from file into users array(split might not work)
-  std::ifstream MyReadFile("Desktop/passwords.txt");
+
+  
+
+  
+
+  ifstream MyReadFile("passwords.txt");
 
 
-  while (getline ((MyReadFile, storedUserData, ":")&& auth == false )) {
+
+
+
+  if(MyReadFile.is_open()){
+
+  while (getline(MyReadFile, storedUserData)){
+
    std:: cout << storedUserData;//TODO only usenames read in 
+
   }
+
+
 
   MyReadFile.close();
-  while(i<sizeof(storedUserData)){
-  while(loginAttempt < 3){
-   std:: cout << "please enter usename: ";
-    std::cin >> user;
-   std:: cout << "please enter password: ";
-    std::cin >> userPassword;
-    auth == true;  
-  if(  user != storedUserData){ //compare users entered to users from file
-    auth == false;     
-    i++
-  }
-  }
-  loginAttempt = loginAttempt +1; //add to sign in attempt
+
+  while(i<sizeof(storedUserData)&& auth == false){
+
+    while(loginAttempt < 3){
+
+      std:: cout << "please enter usename: ";
+
+      std::cin >> user;
+
+      std:: cout << "please enter password: ";
+
+     std::cin >> userPassword;
+
+     auth == true;  
+
+      if(  user != storedUserData){ //compare users entered to users from file
+
+         auth == false;     
+
+          i++;
+
+      }
+
+      loginAttempt = loginAttempt +1;
+
+    }   
+
+  } //add to sign in attempt
+
  //todo read in password when users are correct  
+
  //TODO optional after rest is done salt before hashing
+
  //todo hash the passwords 
+
  //to do compare passwords 
+
   if (auth) authenticated("user");
+
  //sign in
+
   else rejected("user");
+
   }
+
+  else cout << "unable to open file";
+
 }
