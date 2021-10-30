4c1844b4f1393e881ccae3ebbaa19feb88cf0bcc
JackG789 <55397601+JackG789@users.noreply.github.com>
2021-10-30 18:29:15 +0100

    Update login.cpp
    fixing shit

--- Assignment 1 ZIP/Assignment 1 ZIP/login.cpp
+++ Assignment 1 ZIP/Assignment 1 ZIP/login.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
+#include <iostream>
 #include "authlib.h"
 
 int main() {
-  string user;
-  string userPassword;
+  std::string user;
+  std::string userPassword;
   string storedUserData[];
+  std::int i;
   
-  bool auth = true;
+  bool auth = false;
   int loginAttempt =0;
   //todo read names from file into users array(split might not work)
-  ifstream MyReadFile("users.txt");
-  while (getline (MyReadFile, storedUserData, ":")) {
-  cout << storedUserData;//TODO only usenames read in 
+  std::ifstream MyReadFile("Desktop/passwords.txt");
 
+
+  while (getline ((MyReadFile, storedUserData, ":")&& auth == false )) {
+   std:: cout << storedUserData;//TODO only usenames read in 
   }
-  MyReadFile.close();
 
+  MyReadFile.close();
+  while(i<sizeof(storedUserData)){
   while(loginAttempt < 3){
-    cout << "please enter usename: ";
-    cin >> user;
-    cout << "please enter password: ";
-    cin >> userPassword;
-  if(  user != storedUserData[]){ //compare users entered to users from file
+   std:: cout << "please enter usename: ";
+    std::cin >> user;
+   std:: cout << "please enter password: ";
+    std::cin >> userPassword;
+    auth == true;  
+  if(  user != storedUserData){ //compare users entered to users from file
     auth == false;     
+    i++
+  }
   }
   loginAttempt = loginAttempt +1; //add to sign in attempt
  //todo read in password when users are correct  
+ //TODO optional after rest is done salt before hashing
  //todo hash the passwords 
  //to do compare passwords 
   if (auth) authenticated("user");
