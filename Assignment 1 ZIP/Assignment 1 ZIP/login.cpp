87ee4ac39e381d6923748d9ea9874fd50cc4fe7f
JackG789 <55397601+JackG789@users.noreply.github.com>
2021-10-31 15:43:17 +0000

    Update login.cpp
    line update

--- Assignment 1 ZIP/Assignment 1 ZIP/login.cpp
+++ Assignment 1 ZIP/Assignment 1 ZIP/login.cpp
@@ -1,75 +1,146 @@
 #include <iostream>
+
 #include "authlib.h"
+
 #include <fstream>
+
 #include<vector>
 
 using namespace std;
 
 
 
-int main() {
+int main()
+
+{
+
+
+
   string user;
+
   string userPassword;
-  string storedUserData;
-  int i=0;
+
+  int i = 0;
+
   bool auth = false;
-  int loginAttempt =0;
+
+  int loginAttempt = 0;
+
+
+
   string line;
+
   std::vector<std::string> lines;
-  ifstream MyReadFile ("passwords.txt");
+
+  ifstream MyReadFile("passwords.txt");
+
+
 
   if (MyReadFile.is_open())
-  {
-     for (std::string line; std::getline(MyReadFile, line, ':');) 
-        {
-            lines.push_back(line); //adds to end of the vector
 
-        }
+  {
+
+    for (std::string line; std::getline(MyReadFile, line, ':');)
+
+    {
+
+      lines.push_back(line); //adds to end of the vector
+
+    }
+
     MyReadFile.close();
+
   }
 
-  
-  while(i<sizeof(storedUserData)&& auth == false){
 
-    while(loginAttempt < 3){
 
-      std:: cout << "please enter usename: ";
+  while (i < sizeof(line) && auth == false)
 
-      std::cin >> user;
+  {
 
-      std:: cout << "please enter password: ";
 
-     std::cin >> userPassword;
 
-     auth == true;  
+    while (loginAttempt < 3)
 
-      if(  user != storedUserData){ //compare users entered to users from file
+    {
 
-         auth == false;     
 
-          i++;
+
+      cout << "please enter usename: ";
+
+
+
+      cin >> user;
+
+
+
+      cout << "please enter password: ";
+
+
+
+      cin >> userPassword;
+
+
+
+      auth == true;
+
+
+
+      if (user != line)
+
+      { 
+
+        auth == false;
+
+        i++;
 
       }
 
-      loginAttempt = loginAttempt +1;
 
-    }   
 
-  } //add to sign in attempt
+      loginAttempt = loginAttempt + 1;
 
- //todo read in password when users are correct  
+    }
 
- //TODO optional after rest is done salt before hashing
 
- //todo hash the passwords 
 
- //to do compare passwords 
+  } 
 
-  if (auth) authenticated("user");
 
- //sign in
 
-  else rejected("user");
+  //todo read in password when users are correct
 
-  }
+
+
+  //TODO optional after rest is done salt before hashing
+
+
+
+  //todo hash the passwords
+
+
+
+  //to do compare passwords
+
+
+
+  if (auth)
+
+    authenticated("user");
+
+
+
+  //sign in
+
+
+
+  else
+
+    rejected("user");
+
+
+
+}
+
+
 
