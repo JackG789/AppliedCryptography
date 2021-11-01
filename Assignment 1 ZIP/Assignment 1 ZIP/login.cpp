d355ee1142e740197f6750c8b03159c26816723f
JackG789 <55397601+JackG789@users.noreply.github.com>
2021-11-01 20:06:51 +0000

    Update login.cpp
    yes

--- Assignment 1 ZIP/Assignment 1 ZIP/login.cpp
+++ Assignment 1 ZIP/Assignment 1 ZIP/login.cpp
@@ -1,156 +1,93 @@
-#include <iostream>
+//Delta10z
 #include "authlib.h"
-#include <fstream>
-#include<vector>
-#include <iomanip>
-#include <sstream>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <ctime>
 #include <openssl/sha.h>
 
 using namespace std;
 
-string sha256(const string str)
+string user,password; //initialise 
+
+string sha256(const string password)//hashing in sha256 function
 {
-    unsigned char hash[SHA256_DIGEST_LENGTH];
+    unsigned char hash[SHA256_DIGEST_LENGTH]; 
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
-    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Update(&sha256, password.c_str(), password.size());
     SHA256_Final(hash, &sha256);
-    stringstream ss;
-    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    stringstream ourStream;
+
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)//hashing each charcater
     {
-        ss << hex << setw(2) << setfill('0') << (int)hash[i];
+        ourStream << hex << setw(2) << setfill('0') << (int)hash[i];
     }
-    return ss.str();
+    return ourStream.str();
 }
 
-int main()
 
+vector<string> readFromFile(){//reading from the passwords file
+	vector<string> savedDetails; //vector storing usernames & passwords
+	fstream readFile;
+	readFile.open("passwords.txt", ios::in); //read passwords.txt
+	string currentLine;
+	if(readFile.is_open()){
+		while (getline(readFile, currentLine)){ //if there is data read it in 
+			savedDetails.push_back(currentLine); //add to the vector
+		}
+		readFile.close(); 
+	}
+	else {
+		cout << "unable to open file" << endl; //error handiling 
+	}
+	return savedDetails; //Return the vector 
+}
+
+
+
+void compare(string hashedPassword, vector<string> savedDetails) //function to checking to see if the usernames and hashed passwords line up 
 {
-
-
-
-  string user;
-
-  string userPassword;
-
-  int i = 0;
-
-  bool auth = false;
-
-  int loginAttempt = 0;
-
-
-
-  string line;
-
-  std::vector<std::string> lines;
-
-  ifstream MyReadFile("passwords.txt");
-
-
-
-  if (MyReadFile.is_open())
-
-  {
-
-    for (std::string line; std::getline(MyReadFile, line, ':');)
-
-    {
-
-      lines.push_back(line); //adds to end of the vector
-
-    }
-
-    MyReadFile.close();
-
-  }
-
-
-
-  while (i < sizeof(lines) && auth == false)
-
-  {
-
-
-
-    while (loginAttempt < 3)
-
-    {
-
-
-
-      cout << "please enter usename: ";
-
-
-
-      cin >> user;
-
-
-
-      cout << "please enter password: ";
-
-
-
-      cin >> userPassword;
-
-
-      if (user != line)
-      { 
-        auth == false;
-        i++;
-      }
-
-
-      if //stored hash == sha256(userPassword)
-      {
-        auth = true;
-      }
-
-    
-
-      loginAttempt = loginAttempt + 1;
-
-    }
-
-
-
-  } 
-
-
-
-  //todo read in password when users are correct
-
-
-
-  //TODO optional after rest is done salt before hashing
-
-
-
-  //todo hash the passwords
-
-
-
-  //to do compare passwords
-
-
-
-  if (auth)
-
-    authenticated("user");
-
-
-
-  //sign in
-
-
-
-  else
-
-    rejected("user");
-
-
-
+	vector<string>::iterator it; //Create an iterator to go through the vectore one at a time 
+	string enteredLogin = user + ":" + hashedPassword; //Put user input into same format as text file
+	it = find (savedDetails.begin(), savedDetails.end(), enteredLogin); //search vector "savedDetails" for user entry
+
+	if(it != savedDetails.end()){ //If not at the end of the vector
+		authenticated(user); //Authenticate user
+		exit(0); //terminates program
+	}
+	else {
+		rejected(user); //Reject user
+	}
 }
 
 
+//Method to get the user to enter their username and password   
+void getLogin(){
+	cout << "Please enter your username :" << endl; //Get username
+	cin >> user;
+
+	cout << "Please enter your password :" << endl; //Get password
+	cin >> password;
+
+	string hashedPasswordassword = sha256(p); //hash password entered by the user
+	vector<string> savedDetails = readFromFile(); //read in saved usernames and passwords from text file
+	compare(hashedPasswordassword, savedDetails);//Compare the saved details and the user entry
+}
+
+
+int main() {
+	int counter = 0;
+	while(counter < 3){ //While user has not had 3 attempts at logging in
+		getLogin();
+		counter++;
+	}
+
+	if (counter == 3){ //If user has entered details incorrectly 3 times display error message and close program
+		cout <<"Sorry - maximum amount of tries." << endl;
+		exit(0); //terminates program
+	}
+}
